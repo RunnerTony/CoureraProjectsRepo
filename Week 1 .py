6848ce470e6f3c2b7e6c325230ae18c71a5a344d
RunnerTony <58478887+RunnerTony@users.noreply.github.com>
2020-02-01 20:34:51 +0300

    Add files via upload

---
+++ Week 1 .py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mailnly used for the capstone project for the Applied Data Science Capstone course on Coursera.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
